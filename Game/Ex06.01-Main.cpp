965fa1e55f91e788bff7a091e73e852387d0d9a7
Alex Torrents Sánchez <atorrents@enti.local>
2016-09-16 19:25:27 +0200

    test Read from JSON file

--- Game/Ex06.01-Main.cpp
+++ Game/Ex06.01-Main.cpp
@@ -2,10 +2,15 @@
 
 #include "Game.h"
 
+#include "SceneCreator.h"
+
 int main(int argc, char ** argv) {
 	// Hide the command line
 	//ShowWindow(GetConsoleWindow(), SW_HIDE);
-	
+	SceneCreator sceneCr;
+
+	sceneCr.createScene("./resources/Scenes/Scene1.json");
+
 	Game game("BattleBots", 1280,  800,true,60,false);
 	try {
 		game.start();
--- Game/OpenGL.cpp
+++ Game/OpenGL.cpp
@@ -33,6 +33,8 @@ void OpenGL::initializeBuffers() {
 	initializeVertexArrayObject();
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
 }
 
 

bc540ff201490fa75a9069526d77dcd2eff1ee1f
Alex Torrents Sánchez <atorrents@enti.local>
2016-09-26 18:48:03 +0200

    new GameLoop class

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -8,20 +8,18 @@
 int main(int argc, char ** argv) {
 	// Hide the command line
 	//ShowWindow(GetConsoleWindow(), SW_HIDE);
-	SceneCreator sceneCr;
 
-	sceneCr.createScene("./resources/Scenes/Scene1.json");
+	//Game game("BattleBots", 1280,  800,true,60,false);
+	TurriFramework::Instance().init("BattleBots", 1280, 800, true, 60, false);
 
-	Game game("BattleBots", 1280,  800,true,60,false);
-
-	//GameController::getInstance().init("BattleBots", 1280, 800, true, 60, false);
+	GameController::getInstance().init("BattleBots", 1280, 800, true, 60, false);
 	try {
-		game.start();
-		/*while(GameController::getInstance().isRunning()) {
+		//game.start();
+		while(GameController::getInstance().isRunning()) {
 			GameController::getInstance().input();
 			GameController::getInstance().update();
 			GameController::getInstance().render();
-		}*/
+		}
 	}
 	catch (std::exception e) {
 		std::cerr << "ERROR: " << e.what() << std::endl;

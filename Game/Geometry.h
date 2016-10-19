1f7671483483153914a776b256eda0e0c124a689
Alex Torrents Sánchez <atorrents@enti.local>
2016-10-19 19:26:27 +0200

    implementing Assimp loader fbx

--- Game/Geometry.h
+++ Game/Geometry.h
@@ -11,4 +11,5 @@ public:
 	static  std::vector<GameObject> LoadGameElements(std::string file);
 
 	static OBJ LoadModelFromFile(std::string file);
+	static OBJ LoadModelFBXFromFile(std::string file);
 };

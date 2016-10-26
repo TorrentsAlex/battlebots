5fdee16c59969fc51cebd1765adae8e12bc09302
Alex Torrents Sánchez <atorrents@enti.local>
2016-10-26 16:27:09 +0200

    loading dwarf model with fbx file, not succesfully

--- Game/Geometry.cpp
+++ Game/Geometry.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #include <assimp/vector3.h>
 
 OBJ Geometry::LoadModelFBXFromFile(std::string file) {
-	OBJ newOBJ;
+	OBJ fbxObject;
 	
 	// Create an instance of the Importer class
 	Assimp::Importer importer;
@@ -28,50 +28,54 @@ OBJ Geometry::LoadModelFBXFromFile(std::string file) {
 	if (!scene) {
 		cout << "error loading file: " << file << " Error:"
 			<< importer.GetErrorString() << endl;
-		return newOBJ;
+		return fbxObject;
 	}
-	std::vector<Vertex> vertices;
-	std::vector<Vertex> indices;
 
 	// get ONLY the first mesh 
 	const aiMesh* mesh = scene->mMeshes[0];
 
 	// init a zero3D vector if not exist some values
 	const aiVector3D Zero3D(0.0f, 0.0f, 0.0f);
+	fbxObject.numVertices = mesh->mNumVertices;
+	// Load all vertexs, normal, and texture coordinates
 	for (int i = 0; i < mesh->mNumVertices; i++) {
 
 		const aiVector3D* pPos = &(mesh->mVertices[i]);
 		const aiVector3D* pNormal = mesh->HasNormals() ? &(mesh->mNormals[i]): &Zero3D;
-	const aiVector3D* pTexCoord = mesh->HasTextureCoords(0) ? &(mesh->mTextureCoords[0][i]) : &Zero3D;
-	/*
-		Vertex v(Vector3f(pPos->x, pPos->y, pPos->z),
-			Vector2f(pTexCoord->x, pTexCoord->y),
-			Vector3f(pNormal->x, pNormal->y, pNormal->z));
+		const aiVector3D* pTexCoord = mesh->HasTextureCoords(0) ? &(mesh->mTextureCoords[0][i]) : &Zero3D;
+		
+		fbxObject.vertexs.push_back(glm::vec3(pPos->x, pPos->y, pPos->z));
+		fbxObject.vertex_normals.push_back(glm::vec3(pNormal->x, pNormal->y, pNormal->z));
+		fbxObject.textures_coord.push_back(glm::vec3(pTexCoord->x, pTexCoord->y, pTexCoord->z));
+	
+	}
+	
+	// Load the faces of the model
+	for (int i = 0; i < mesh->mNumFaces; i++) {
+		const aiFace& face = mesh->mFaces[i];
+		assert(face.mNumIndices == 3);
+		fbxObject.faces.push_back(face.mIndices[0]);
 
-		vertices.push_back(v);
-	*/}
-	/*
-	    for (unsigned int i = 0 ; i < paiMesh->mNumVertices ; i++) {
-        const aiVector3D* pPos = &(paiMesh->mVertices[i]);
-        const aiVector3D* pNormal = &(paiMesh->mNormals[i]) : &Zero3D;
-        const aiVector3D* pTexCoord = paiMesh->HasTextureCoords(0) ? &(paiMesh->mTextureCoords[0][i]) : &Zero3D;
+		// And load the model
+		fbxObject.uv.push_back(face.mIndices[1]);
 
-        Vertex v(Vector3f(pPos->x, pPos->y, pPos->z),
-                Vector2f(pTexCoord->x, pTexCoord->y),
-                Vector3f(pNormal->x, pNormal->y, pNormal->z));
+		fbxObject.normals.push_back(face.mIndices[2]);
+	}
 
-        Vertices.push_back(v);
-    }
-    ...
-Here we prepare the contents of the vertex buffer by populating the Vertices vector. We use the following attributes of the aiMesh class:
-mNumVertices - the number of vertices.
-mVertices - an array of mNumVertices vectors that contain the position.
-mNormals - an array of mNumVertices vectors that contain the vertex normals.
-mTextureCoords - an array of mNumVertices vectors that contain the texture coordinates. 
-This is actualy a two dimensional array because each vertex can hold several texture coordinates.
-	*/
-	// We're done. Everything will be cleaned up by the importer destructor
-	return newOBJ;
+	// Load the MESH
+
+	fbxObject.mesh = new Vertex[fbxObject.faces.size()];
+	for (int i = 0; i < fbxObject.faces.size(); i++) {
+		float vPos = fbxObject.faces.at(i);
+		float vUV = fbxObject.uv.at(i);
+		float vNorm = fbxObject.normals.at(i);
+
+		fbxObject.mesh[i].setPosition(fbxObject.vertexs.at(vPos).x, fbxObject.vertexs.at(vPos).y, fbxObject.vertexs.at(vPos).z);
+		fbxObject.mesh[i].setUV(fbxObject.textures_coord.at(vUV).x, fbxObject.textures_coord.at(vUV).y);
+		fbxObject.mesh[i].setNormal(fbxObject.vertex_normals.at(vNorm).x, fbxObject.vertex_normals.at(vNorm).y, fbxObject.vertex_normals.at(vNorm).z);
+	}
+
+	return fbxObject;
 }
 
 OBJ Geometry::LoadModelFromFile(std::string file) {
@@ -100,6 +104,7 @@ OBJ Geometry::LoadModelFromFile(std::string file) {
 	} catch (std::exception e) {
 		std::cerr << "ERROR: " << iDebug << " i " << e.what() << std::endl;
 	}
+	// Calculate the max and the minim vertex
 	// We only need to know the vertex, the faces don't 
 	for (int i = 0; i < object.vertexs.size(); i++) {
 		currentVertex = object.vertexs.at(i);
